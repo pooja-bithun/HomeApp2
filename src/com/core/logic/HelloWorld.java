13a35530154d1c0a7f176bb5e02893299af5139b
pooja-bithun <58051440+pooja-bithun@users.noreply.github.com>
2020-01-20 19:35:43 -0500

    first commit

---
+++ src/com/core/logic/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.core.logic;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Hello 2");
+    }
+}
